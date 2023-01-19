9d7d9278cdd54e6971fdb3487f685b4164875cf3
nixuno <11358399+nixuno@users.noreply.github.com>
2023-01-19 15:43:02 -0500

    Add new function: New-SQLiteLogTable

---
+++ Strapper/Private/SQLite/New-SQLiteLogTable.ps1
@@ -0,0 +1,35 @@
+function New-SQLiteLogTable {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][ValidatePattern('^[a-zA-Z0-9\-_]+$')][string]$Name,
+        [Parameter()][System.Data.SQLite.SQLiteConnection]$Connection,
+        [Parameter()][switch]$Clobber,
+        [Parameter()][switch]$PassThru
+    )
+    $targetTable = Get-SQLiteTable -Name $Name -Connection $Connection
+    if ($targetTable -and !$Clobber) {
+        Write-Verbose -Message "Target table '$Name' already exists. Pass -Clobber to overwrite this table."
+    } else {
+        Remove-SQLiteTable -Name $Name -Connection $Connection | Out-Null
+        $createCommand = $Connection.CreateCommand()
+        $createCommand.CommandText = @"
+        CREATE TABLE "$Name" (
+            "id"        INTEGER NOT NULL UNIQUE,
+            "level"     TEXT NOT NULL,
+            "message"   MESSAGE NOT NULL,
+            "timestamp" DATETIME NOT NULL,
+            PRIMARY KEY("id" AUTOINCREMENT)
+        );
+"@
+        $rowsAffected = $createCommand.ExecuteNonQuery()
+        Write-Verbose -Message "Affected row count: $rowsAffected"
+        $targetTable = Get-SQLiteTable -Name $Name -Connection $Connection
+        if(!$targetTable) {
+            Write-Error -Exception ([System.Data.SQLite.SQLiteException]::new([System.Data.SQLite.SQLiteErrorCode]::IoErr, "Failed to create table '$Name'"))
+            return
+        }
+    }
+    if ($PassThru) {
+        return $targetTable
+    }
+}
