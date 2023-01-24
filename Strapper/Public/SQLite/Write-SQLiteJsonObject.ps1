8e97718d0a6dec1e4f66168dbd2f268bdb025895
nixuno <11358399+nixuno@users.noreply.github.com>
2023-01-24 14:36:42 -0500

    Add clobber option and rows affected log

--- Strapper/Public/SQLite/Write-SQLiteJsonObject.ps1
+++ Strapper/Public/SQLite/Write-SQLiteJsonObject.ps1
@@ -4,26 +4,29 @@ function Write-SQLiteJsonObject {
         [Parameter(Mandatory)][ValidatePattern('^[a-zA-Z0-9\-_]+$')][string]$TableName,
         [Parameter()][string]$DataSource = $StrapperSession.DBPath,
         [Parameter(Mandatory, ValueFromPipeline)][System.Object[]]$InputObject,
-        [Parameter()][int]$Depth = 64
+        [Parameter()][int]$Depth = 64,
+        [Parameter()][switch]$Clobber
     )
     begin {
         [System.Data.SQLite.SQLiteConnection]$sqliteConnection = New-SQLiteConnection -DataSource $DataSource -Open
-        New-SQLiteObjectTable -Name $TableName -Connection $sqliteConnection
+        New-SQLiteObjectTable -Name $TableName -Connection $sqliteConnection -Clobber:$Clobber
         $sqliteCommand = $sqliteConnection.CreateCommand()
         $sqliteTransaction = $sqliteConnection.BeginTransaction()
         $sqliteCommand.Transaction = $sqliteTransaction
+        $rowsAffected = 0
     }
     process {
         foreach ($obj in $InputObject) {
             $jsonObjectString = $obj | ConvertTo-Json -Depth $Depth -Compress
             $sqliteCommand.CommandText = "INSERT INTO '$TableName' (json, timestamp) VALUES (:json, (SELECT datetime('now')))"
-            $sqliteCommand.Parameters.AddWithValue(':json', $jsonObjectString)
-            $sqliteCommand.ExecuteNonQuery()
+            $sqliteCommand.Parameters.AddWithValue(':json', $jsonObjectString) | Out-Null
+            $rowsAffected += $sqliteCommand.ExecuteNonQuery()
             $sqliteCommand.Parameters.Clear()
         }
     }
     end {
         $sqliteTransaction.Commit()
+        Write-Verbose -Message "Rows affected: $rowsAffected"
         $sqliteTransaction.Dispose()
         $sqliteConnection.Dispose()
     }
