c3ff4ca37d6b8853c2d133229f1473b7328e92be
nixuno <11358399+nixuno@users.noreply.github.com>
2024-03-14 17:14:32 -0400

    Add tests

--- Strapper.Tests.ps1
+++ Strapper.Tests.ps1
@@ -1,10 +1,56 @@
 BeforeAll {
-    Import-Module "$PSScriptRoot/Strapper/Strapper.psd1"
+    $strapperModuleManifest = Get-ChildItem -Path "$PSScriptRoot/Output/Strapper" -Recurse -File -Filter "Strapper.psd1" | Select-Object -First 1
+    Import-Module -Name $strapperModuleManifest.FullName
+}
+
+AfterAll {
+    Remove-Module -Name Strapper
+}
+
+Describe "New-SQLiteConnectionString" {
+    Context "No parameters" {
+        It "Returns a connection string" {
+            $StrapperSession.DBPath = "$PSScriptRoot/Output/Strapper/Strapper.db"
+            $connectionString = New-SQLiteConnectionString
+            $connectionString | Should -BeOfType System.String
+            $connectionString | Should -Match ([regex]::Escape($StrapperSession.DBPath))
+        }
+    }
+    Context "Passed parameters" {
+        It "Returns a connection string" {
+            $dataSource = "$PSScriptRoot/Output/Strapper/Strapper.db"
+            $connectionString = New-SQLiteConnectionString -Datasource $dataSource
+            $connectionString | Should -BeOfType System.String
+            $connectionString | Should -Match ([regex]::Escape($dataSource))
+        }
+    }
+}
+
+Describe "New-SQLiteConnection" {
+    Context "No -Open switch" {
+        It "Creates a SQLite connection" {
+            $StrapperSession.DBPath = "$PSScriptRoot/Output/Strapper/Strapper.db"
+            [System.Data.SQLite.SQLiteConnection]$connection = New-SQLiteConnection
+            $connection | Should -BeOfType System.Data.SQLite.SQLiteConnection
+            $connection.State | Should -Be ([System.Data.ConnectionState]::Closed)
+            $connection.Dispose()
+        }
+    }
+    Context "-Open switch passed" {
+        It "Creates an open SQLite connection" {
+            $StrapperSession.DBPath = "$PSScriptRoot/Output/Strapper/Strapper.db"
+            [System.Data.SQLite.SQLiteConnection]$connection = New-SQLiteConnection -Open
+            $connection | Should -BeOfType System.Data.SQLite.SQLiteConnection
+            $connection.State | Should -Be ([System.Data.ConnectionState]::Open)
+            $connection.Dispose()
+        }
+    }
 }
 
 Describe "Write-Log" {
     BeforeEach {
         $testGuid = New-Guid
+        $StrapperSession.LogTable = "Strapper_Tests"
     }
     It "Writes a database entry" {
         $StrapperSession.LogsToDB = $true # default value
